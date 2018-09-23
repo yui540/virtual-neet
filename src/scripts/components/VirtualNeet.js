61079e8000a53f8cfb49c4742fc64473ed0904f4
yui540 <tabletennis540@gmail.com>
2018-09-23 19:31:39 +0900

    add: メンバー紹介ページをマークアップ

--- src/scripts/components/VirtualNeet.js
+++ src/scripts/components/VirtualNeet.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Home from '../containers/Home'
+import Member from '../containers/Member'
 
 export default () => (
   <Router>
-    <div className="cowardly-witch">
+    <div className="virtual-neet">
       <Route exact path="/" component={Home} />
+      <Route exact path="/member" component={Member} />
     </div>
   </Router>
 )
